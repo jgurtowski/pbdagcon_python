dd5488e46c349b68ad23affe3d2266ad94b06492
Jim "Double Phoenix" Drake <jdrake@pacificbiosciences.com>
2013-06-10 12:48:16 -0800

    Bug 21296.  Clean up main a bit, add some comments
    [git-p4: depot-paths = "//depot/software/bioinformatics/tools/pbdagcon/": change = 124310]

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -11,34 +11,8 @@
 #include "Alignment.hpp"
 #include "AlnGraphBoost.hpp"
 
-// Prints out the merged graph
-void mergeGraph(Alignment *algs, const std::string backbone) {
-    AlnGraphBoost ag(backbone); 
-    for (int i=0; i < 5; i++) {
-        normalizeGaps(algs[i]);
-        ag.addAln(algs[i]);
-    }
-    ag.mergeNodes();
-    ag.printGraph();
-}
-
-// Checks for a consistent best path, pipe output to sort | uniq -c
-void 
-consistencyCheck(Alignment *algs, std::string backbone, bool normalize) {
-    for (int i = 0; i < 10000; i++) {
-        std::string backbone = "ATATTAGGC";
-        //AlnGraphBoost ag(backbone); 
-        AlnGraphBoost ag(backbone.length()); 
-        for (int i=0;i < 5; i++) {
-            Alignment a = normalize ? normalizeGaps(algs[i]) : algs[i];
-            ag.addAln(a);
-        }
-
-        ag.mergeNodes();
-        std::cout << ag.consensus() << std::endl;
-    }
-}
-
+// Takes an alignment file (currently blasr -m 5) containing a single target, 
+// generates a consensus and prints it to stdout.
 void alnFileConsensus(const std::string alnFile) {
     std::ifstream file(alnFile);
     Alignment aln;
@@ -52,38 +26,11 @@ void alnFileConsensus(const std::string alnFile) {
         ag.addAln(a);
     }
     ag.mergeNodes();
-    //ag.printGraph();
     std::cout << ">" << targetId << std::endl;
     std::cout << ag.consensus() << std::endl;
 }
 
 int main(int argc, char* argv[]) {
-    const std::string backbone = "ATATTAGGC";
-    Alignment *algs = new Alignment[5];
-    
-    algs[0].tstr = "ATATTA---GGC";
-    algs[0].qstr = "ATAT-AGCCGGC";
-
-    algs[1].tstr = "ATATTA-GGC";
-    algs[1].qstr = "ATAT-ACGGC";
-
-    algs[2].tstr = "AT-ATTA--GGC";
-    algs[2].qstr = "ATCAT--CCGGC";
-
-    algs[3].tstr = "ATATTA--G-GC";
-    algs[3].qstr = "ATAT-ACCGAG-";
-
-    algs[4].tstr = "ATATTA---GGC";
-    algs[4].qstr = "ATAT-AGCCGGC";
-
-    for(int i=0; i < 5; i++) {
-        Alignment& ra = algs[i];
-        ra.tstart = 1;
-    }
-
-    //mergeGraph(algs, backbone);
-    //consistencyCheck(algs, backbone, false);
-    //consistencyCheck(algs, backbone, true);
     if (argc == 2)
         alnFileConsensus(argv[1]);
         

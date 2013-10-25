e49024ea8a0fb1ea31489af8457414c832f7c4dd
Jim "Double Phoenix" Drake <jdrake@pacificbiosciences.com>
2013-10-25 09:46:25 -0800

    Bug 24141 Clean up makefiles
    More work on generalizing the rules and variables for portability.

--- src/cpp/pbi.mk
+++ src/cpp/pbi.mk
@@ -1,8 +1,10 @@
-# Defines variables specific to the PBI build env using relative paths
-
-THIRDPARTY ?= ../../../../../ThirdParty
+# Defines some variables specific to the PBI build env using relative paths
+BASEDIR = ../../../../..
+THIRDPARTY = $(BASEDIR)/ThirdParty
+PREBUILT = $(BASEDIR)/smrtanalysis/prebuilt.out
 BOOST := $(THIRDPARTY)/boost/boost_1_47_0
-LOG4CPP := $(THIRDPARTY)/log4cpp/log4cpp-1.0
+LOG4CPP := $(PREBUILT)/log4cpp/log4cpp-1.1/$(OS_STRING2)
+GTEST ?= $(THIRDPARTY)/gtest-1.3.0
 BLASR ?= ../../../../lib/cpp/alignment
 INCDIRS := -I $(BLASR) -I $(BOOST) -I $(LOG4CPP)/include
-LIBDIRS := -L $(BLASR) -L $(BOOST)/stage/linux_gcc/lib -L $(LOG4CPP)/src/.libs
+LIBDIRS := -L $(BLASR) -L $(BOOST)/stage/linux_gcc/lib -L $(LOG4CPP)/lib

f5c3a94f832cda52deef14d4a74d34ff0aae5f08
James Drake <jdrake@pacificbiosciences.com>
2014-09-04 14:45:23 -0700

    Makefile cleanup
    Need a bit more flexibility during builds with the github reorg.

--- Makefile
+++ Makefile
@@ -1,30 +1,23 @@
-.PHONY: all clean install dev-install test
 SHELL = /bin/bash -e
+LIBS =  pbdata/libpbdata.a alignment/libblasr.a
 
-all: install
+all : cpp
+clean : cpp-clean
+test : cpp-check
 
-install:
-	python setup.py install
-
-dev-install:
-	python setup.py develop
-
-clean:
-	rm -rf build/;\
-	find . -name "*.egg-info" | xargs rm -rf;\
-	rm -rf dist/
-
-test:
-	nosetests src/tests/*
-	cram examples/*.t
-	
 # C++ project build directives
-cpp:
+cpp : $(LIBS)
 	$(MAKE) -C src/cpp
 
-cpp-check: cpp
+cpp-check : cpp
 	$(MAKE) -C test/cpp
 
-cpp-clean:
+cpp-clean :
 	$(MAKE) -C src/cpp clean
 	$(MAKE) -C test/cpp clean
+	$(MAKE) -C pbdata clean
+	$(MAKE) -C alignment clean
+
+$(LIBS) : 
+	$(MAKE) -C $(@D) nohdf=1
+
--- src/cpp/pbi.mk
+++ src/cpp/pbi.mk
@@ -1,11 +1,12 @@
 # Defines some variables specific to the PBI build env using relative paths
-BASEDIR = ../../../../../..
-THIRDPARTY = $(BASEDIR)/smrtanalysis/prebuilt.out
+BASEDIR = ../../../../..
+THIRDPARTY = $(BASEDIR)/ThirdParty
 PREBUILT = $(BASEDIR)/smrtanalysis/prebuilt.out
-BOOST := $(THIRDPARTY)/boost/boost_1_47_0
-LOG4CPP := $(PREBUILT)/log4cpp/log4cpp-1.1/$(OS_STRING2)
+BOOST = $(THIRDPARTY)/boost/boost_1_47_0
+LOG4CPP = $(PREBUILT)/log4cpp/log4cpp-1.1/$(OS_STRING2)
 GTEST ?= $(THIRDPARTY)/gtest-1.3.0
-BLASR ?= ../../../../lib/cpp/alignment
-PBDATA ?= ../../../../lib/cpp/pbdata
-INCDIRS := -I $(PBDATA) -I $(BLASR) -I $(BOOST) -I $(LOG4CPP)/include
-LIBDIRS := -L $(PBDATA) -L $(BLASR) -L $(BOOST)/stage/linux_gcc/lib -L $(LOG4CPP)/lib
+PBILIB = ../..
+ALIGN = $(PBILIB)/alignment
+PBDATA = $(PBILIB)/pbdata
+INCDIRS = -I $(PBDATA) -I $(ALIGN) #-I $(BOOST) -I $(LOG4CPP)/include
+LIBDIRS = -L $(PBDATA) -L $(ALIGN) #-L $(BOOST)/stage/linux_gcc/lib -L $(LOG4CPP)/lib
